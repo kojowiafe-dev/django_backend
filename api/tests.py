ffe56b2b5dc1a9fcfb53d7c76f4243108cd578d9
kojowiafe-dev <kojowiafe502@gmail.com>
2025-08-06 12:05:36 +0000

    testing

--- api/tests.py
+++ api/tests.py
@@ -72,7 +72,7 @@ class SermonModelTest(TestCase):
 
 class ChurchRoleTest(TestCase):
     def setUp(self):
-        self.admin = User.objects.create_user(username='admin', email='admin@gmail.com', date_of_birth='2025-08-05', password='200509', role='admin')
+        self.admin = User.objects.create_user(username='admin', email='admin@gmail.com', date_of_birth='2025-08-06', password='200509', role='admin')
         self.member = User.objects.create_user(username='member', email='member@gmail.com', password='200509', role='member')
         self.pastor = User.objects.create_user(username='pastor', email='pastor@gmail.com', password='200509', role='pastor')
         self.sermon = Sermon.objects.create(title='Faith', creator=self.pastor)
